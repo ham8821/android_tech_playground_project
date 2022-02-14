f58b74ac6d427ff36e1045eafa4e17390389be8c
Emma Ham <ham38538821@gmail.com>
2022-02-14 21:07:57 +1300

    viewbinding added for view referencing
    1. view binding enabled in the build.gradle
    2. created layout file in res
    3. binding the created binding class in TransactionsFragment

--- app/src/main/java/nz/co/test/transactions/ui/fragments/TransactionListFragment.kt
+++ app/src/main/java/nz/co/test/transactions/ui/fragments/TransactionListFragment.kt
@@ -1,16 +1,30 @@
 package nz.co.test.transactions.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import nz.co.test.transactions.databinding.FragmentTransactonListBinding
 
 class TransactionListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    // View Binding
+    private var _binding: FragmentTransactonListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTransactonListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

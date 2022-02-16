82c1ef8e10e2ec5a67afd3b0c647c221099e7d2d
Emma Ham <ham38538821@gmail.com>
2022-02-16 20:19:21 +1300

    espresso test added

--- app/src/androidTest/java/nz/co/test/transactions/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/nz/co/test/transactions/ExampleInstrumentedTest.kt
@@ -1,12 +1,36 @@
 package nz.co.test.transactions
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import nz.co.test.transactions.ui.activities.MainActivity
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.espresso.util.HumanReadables
 
-import org.junit.Assert.*
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.UiController
+
+import androidx.test.espresso.util.TreeIterables
+
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+
+import androidx.test.espresso.ViewAction
+import org.hamcrest.Matcher
+import java.util.concurrent.TimeoutException
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +39,69 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule @JvmField
+    val mActivityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("nz.co.test.transactions", appContext.packageName)
+    }
+
+    @Test
+    fun assertMainScreenTitle() {
+        // Context of the app under test.
+        onView(withId(R.id.header_title))
+            .check(ViewAssertions.matches(withText("Transaction list")))
+    }
+
+    @Test
+    fun assert_navigation_from_main_to_detail_screen() {
+        // Context of the app under test.
+        onView(withId(R.id.header_title))
+            .check(ViewAssertions.matches(withText("Transaction list")))
+
+        onView(isRoot()).perform(waitId(R.id.transaction_item_view, 5000));
+
+        onView(withId(R.id.transaction_list)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+
+        onView(withId(R.id.detail_header_title))
+            .check(ViewAssertions.matches(withText("Transaction Detail")))
+
+    }
+
+    fun waitId(viewId: Int, millis: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for a specific view with id <$viewId> during $millis millis."
+            }
+
+            override fun perform(uiController: UiController, view: View?) {
+                uiController.loopMainThreadUntilIdle()
+                val startTime = System.currentTimeMillis()
+                val endTime = startTime + millis
+                val viewMatcher: Matcher<View> = withId(viewId)
+                do {
+                    for (child in TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return
+                        }
+                    }
+                    uiController.loopMainThreadForAtLeast(50)
+                } while (System.currentTimeMillis() < endTime)
+                throw PerformException.Builder()
+                    .withActionDescription(this.description)
+                    .withViewDescription(HumanReadables.describe(view))
+                    .withCause(TimeoutException())
+                    .build()
+            }
+        }
     }
 }

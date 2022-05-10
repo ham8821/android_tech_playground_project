a42b30f894b2a783dd0b5ef55d28efcb4bd327d9
Emma Ham <48266431+ham8821@users.noreply.github.com>
2022-05-11 10:31:43 +1200

    update MainActivity.kt to be able to address a need for viewmodel dependency injection

--- app/src/main/java/nz/co/test/transactions/ui/activities/MainActivity.kt
+++ app/src/main/java/nz/co/test/transactions/ui/activities/MainActivity.kt
@@ -1,15 +1,30 @@
 package nz.co.test.transactions.ui.activities
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import nz.co.test.transactions.R
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import nz.co.test.transactions.ui.compose.TaskListScreenView
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            val navController = rememberNavController()
+
+            NavHost(navController, startDestination = "taskList") {
+                composable("taskList") {
+                    // option #1 create a component inside NavBackStackEntry,
+                    // which can be helpful if we need to provide more than one object from DI here
+                   TODO("viewmodel di injection here")
+                    TaskListScreenView(viewModel = , navController = navController)
+                }
+            }
+        }
     }
 
     companion object{

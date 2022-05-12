3cd770a600bd4bd786c6a45d0bb55f050bd91acc
Emma Ham <48266431+ham8821@users.noreply.github.com>
2022-05-12 22:20:33 +1200

    new colours added

---
+++ app/src/main/java/nz/co/test/transactions/ui/Color.kt
@@ -0,0 +1,26 @@
+package nz.co.test.transactions.ui
+
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import kotlin.coroutines.coroutineContext
+
+/*  Compose theme colors */
+val black1 = Color(0xFF222222)
+val black2 = Color(0xFF000000)
+
+val white = Color(0xFFFFFFFF)
+
+val orange_200 = Color(0xFFfd93ad)
+val orange_300 = Color(0xFFfb698c)
+val orange_400 = Color(0xFFf84a73)
+val orange_500 = Color(0xFFf5315b)
+val orange_600 = Color(0xFFe42c59)
+
+val warm_grey = Color(0xFFd9c7bf)
+val dark_grey = Color(0xFF544e50)
+val light_grey = Color(0xFFe9e3e3)
+
+val RedErrorDark = Color(0xFFB00020)
+val RedErrorLight = Color(0xFFEF5350)
--- app/src/main/java/nz/co/test/transactions/ui/Themes.kt
+++ app/src/main/java/nz/co/test/transactions/ui/Themes.kt
@@ -5,17 +5,50 @@ import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import nz.co.test.transactions.R
 
 /* implementation of MaterialTheme (in this case SpaceAppTheme) can be defined.*/
+private val LightThemeColors = lightColors(
+    primary = orange_500,
+    primaryVariant = orange_300,
+    onPrimary = black2,
+    secondary = Color.White,
+    secondaryVariant = warm_grey,
+    onSecondary = black2,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = white,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = black2,
+)
+
+private val DarkThemeColors = darkColors(
+    primary = orange_600,
+    primaryVariant = Color.White,
+    onPrimary = Color.White,
+    secondary = black1,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = black1,
+    onSurface = Color.White,
+)
+
 @Composable
-fun playgroundTheme(
+fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
 
-    val colors = if (!useDarkTheme) LightColors else DarkColors
+    val colors = if (!useDarkTheme) LightThemeColors else DarkThemeColors
     val shapes = Shapes(
         small = RoundedCornerShape(percent = 50),
         medium = RoundedCornerShape(0f),
@@ -29,7 +62,8 @@ fun playgroundTheme(
 
     MaterialTheme(
         colors = colors,
-        shapes = shapes,
-        content = content
-    )
+        shapes = shapes
+    ) {
+        content()
+    }
 }

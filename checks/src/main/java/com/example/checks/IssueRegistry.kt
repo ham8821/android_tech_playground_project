3ced50d3004534190cbcc6bcd8bc02fdce5d6694
Emma Ham <48266431+ham8821@users.noreply.github.com>
2022-06-07 14:24:34 +1200

    adding ktlint plugin

--- checks/src/main/java/com/example/checks/IssueRegistry.kt
+++ checks/src/main/java/com/example/checks/IssueRegistry.kt
@@ -4,12 +4,6 @@ import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 
-/**
- * The class contains the list of issues that will be checked when running lint.
- *
- * This class is referenced from the `checks/build.gradle` file, so we can
- * suppress the unused warning.
- */
 @Suppress("unused")
 class IssueRegistry : IssueRegistry() {
 

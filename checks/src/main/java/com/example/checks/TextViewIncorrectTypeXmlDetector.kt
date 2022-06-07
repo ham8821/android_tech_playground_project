bf41092754ce0ccc1321a6ec3612965a65c2ae3b
Emma Ham <48266431+ham8821@users.noreply.github.com>
2022-06-07 14:55:36 +1200

    adding editorConfig for disabling wildcard check in ktlint

--- checks/src/main/java/com/example/checks/TextViewIncorrectTypeXmlDetector.kt
+++ checks/src/main/java/com/example/checks/TextViewIncorrectTypeXmlDetector.kt
@@ -1,7 +1,12 @@
 package com.example.checks
 
 import com.android.resources.ResourceFolderType
-import com.android.tools.lint.detector.api.*
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.ResourceXmlDetector
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Scope
 import org.w3c.dom.Element
 
 class TextViewIncorrectTypeXmlDetector : ResourceXmlDetector() {
